613603d2490855854c8f9e98914512718b532c4d
Peter Kang <peeterkang@gmail.com>
2020-02-05 10:18:46 -0800

    features added

--- script.js
+++ script.js
@@ -3,58 +3,68 @@ let formatDate = (currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/
 $("#currentDate").html(formatDate);
 
 
-$("#searchBtn").click(function () {
+$(".btns").click(function () {
 
     let city = "Los Angeles, California"
 
-    if ($("#citySearch").val() !== "") {
-        city = $("#citySearch").val();
+    if ($(".citySearch").val() !== "") {
+        city = $(".citySearch").val();
     }
 
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=27254921fe98837775faa360d2cb4843`
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            console.log(response);
-            $(".city").append(response.name);
-            $(".wind").append("Wind Speed: " + response.wind.speed + " mph & " + response.wind.deg + " degrees");
-            $(".humidity").append("Humidity: " + response.main.humidity + "%");
-            $(".temp").append("Temperature: " + Math.round((((response.main.temp) - 273.15) * 1.80 + 32)) + " F");
-            ((response.main.temp) - 273.15) * 1.80 + 32
-        });
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                $(".city").append(response.name);
+                $(".wind").append("Wind Speed: " + response.wind.speed + " mph & " + response.wind.deg + " degrees");
+                $(".humidity").append("Humidity: " + response.main.humidity + "%");
+                $(".temp").append("Temperature: " + Math.round((((response.main.temp) - 273.15) * 1.80 + 32)) + " F");
+                ((response.main.temp) - 273.15) * 1.80 + 32
+            });
 
     let secondQueryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=27254921fe98837775faa360d2cb4843`
 
-    $.ajax({
-        url: secondQueryURL,
-        method: "GET"
-    })
-        .then(function (response) {
-            console.log(response);
-            console.log(Math.round(response.list[0].main.temp))
-            $(".firstDate").append(response.list[0].dt_txt);
-            $(".secondDate").append(response.list[7].dt_txt);
-            $(".thirdDate").append(response.list[15].dt_txt);
-            $(".fourthDate").append(response.list[23].dt_txt);
-            $(".fifthDate").append(response.list[31].dt_txt);
+        $.ajax({
+            url: secondQueryURL,
+            method: "GET",
+        })
+            .then(function (response) {
+                console.log(response);
+                $(".firstDate").append(response.list[0].dt_txt);
+                $(".secondDate").append(response.list[7].dt_txt);
+                $(".thirdDate").append(response.list[15].dt_txt);
+                $(".fourthDate").append(response.list[23].dt_txt);
+                $(".fifthDate").append(response.list[31].dt_txt);
+                $(".firstTemp").append("Temp: " + Math.round((((response.list[0].main.temp) - 273.15) * 1.80 + 32)) + " F");
+                $(".secondTemp").append("Temp: " + Math.round((((response.list[7].main.temp) - 273.15) * 1.80 + 32)) + " F");
+                $(".thirdTemp").append("Temp: " + Math.round((((response.list[15].main.temp) - 273.15) * 1.80 + 32)) + " F");
+                $(".fourthTemp").append("Temp: " + Math.round((((response.list[23].main.temp) - 273.15) * 1.80 + 32)) + " F");
+                $(".fifthTemp").append("Temp: " + Math.round((((response.list[31].main.temp) - 273.15) * 1.80 + 32)) + " F");
+                $(".firstHum").append("Humidity: " + response.list[0].main.humidity + "%");
+                $(".secondHum").append("Humidity: " + response.list[7].main.humidity + "%");
+                $(".thirdHum").append("Humidity: " + response.list[15].main.humidity + "%");
+                $(".fourthHum").append("Humidity: " + response.list[23].main.humidity + "%");
+                $(".fifthHum").append("Humidity: " + response.list[31].main.humidity + "%");
+            });
+    
+    localStorage.setItem(city, city);   
 
-            $(".firstTemp").append("Temp: " + Math.round((((response.list[0].main.temp) - 273.15) * 1.80 + 32)) + " F");
-            $(".secondTemp").append("Temp: " + Math.round((((response.list[7].main.temp) - 273.15) * 1.80 + 32)) + " F");
-            $(".thirdTemp").append("Temp: " + Math.round((((response.list[15].main.temp) - 273.15) * 1.80 + 32)) + " F");
-            $(".fourthTemp").append("Temp: " + Math.round((((response.list[23].main.temp) - 273.15) * 1.80 + 32)) + " F");
-            $(".fifthTemp").append("Temp: " + Math.round((((response.list[31].main.temp) - 273.15) * 1.80 + 32)) + " F");
-
-            $(".firstHum").append("Humidity: " + response.list[0].main.humidity + "%");
-            $(".secondHum").append("Humidity: " + response.list[7].main.humidity + "%");
-            $(".thirdHum").append("Humidity: " + response.list[15].main.humidity + "%");
-            $(".fourthHum").append("Humidity: " + response.list[23].main.humidity + "%");
-            $(".fifthHum").append("Humidity: " + response.list[31].main.humidity + "%");
-        });
+    function renderHistory (){
+        let history = $("<button>");
+        history.attr("class", "btns");
+        let itemInfo = localStorage.getItem(city)
+        history.text(itemInfo)
+        $(".historyList").append(history);
+    }
+    renderHistory();
     emptyInfo();
 });
+
+
+
 
 function emptyInfo() {
     $(".city").empty();
