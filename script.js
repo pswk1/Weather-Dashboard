ec4629a24ecac1a0a36dcd6287c50c13e0098e06
Peter Kang <peeterkang@gmail.com>
2020-02-04 20:43:00 -0800

    functionality improved

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
+let currentDate = new Date();
+let formatDate = (currentDate.getMonth() + 1) + "/" + currentDate.getDate() + "/" + currentDate.getFullYear()
+$("#currentDate").html(formatDate);
+
 
 $("#searchBtn").click(function () {
 
     let city = "Los Angeles, California"
 
     if ($("#citySearch").val() !== "") {
-    city = $("#citySearch").val();
-    } 
+        city = $("#citySearch").val();
+    }
 
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=27254921fe98837775faa360d2cb4843`
-    
+
     $.ajax({
         url: queryURL,
         method: "GET"
-      })
-      .then(function(response) {
-        console.log(response);
-        $(".city").append(response.name);
-        $(".wind").append("Wind Speed: " + response.wind.speed + " mph & " + response.wind.deg + " degrees");
-        $(".humidity").append("Humidity: " + response.main.humidity + "%");
-        $(".temp").append("Temperature (F): " + (((response.main.temp)-273.15) * 1.80 + 32));
-        ((response.main.temp)-273.15) * 1.80 + 32
-    });
+    })
+        .then(function (response) {
+            console.log(response);
+            $(".city").append(response.name);
+            $(".wind").append("Wind Speed: " + response.wind.speed + " mph & " + response.wind.deg + " degrees");
+            $(".humidity").append("Humidity: " + response.main.humidity + "%");
+            $(".temp").append("Temperature: " + Math.round((((response.main.temp) - 273.15) * 1.80 + 32)) + " F");
+            ((response.main.temp) - 273.15) * 1.80 + 32
+        });
+
+    let secondQueryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=27254921fe98837775faa360d2cb4843`
+
+    $.ajax({
+        url: secondQueryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response);
+            console.log(Math.round(response.list[0].main.temp))
+            $(".firstDate").append(response.list[0].dt_txt);
+            $(".secondDate").append(response.list[7].dt_txt);
+            $(".thirdDate").append(response.list[15].dt_txt);
+            $(".fourthDate").append(response.list[23].dt_txt);
+            $(".fifthDate").append(response.list[31].dt_txt);
+
+            $(".firstTemp").append("Temp: " + Math.round((((response.list[0].main.temp) - 273.15) * 1.80 + 32)) + " F");
+            $(".secondTemp").append("Temp: " + Math.round((((response.list[7].main.temp) - 273.15) * 1.80 + 32)) + " F");
+            $(".thirdTemp").append("Temp: " + Math.round((((response.list[15].main.temp) - 273.15) * 1.80 + 32)) + " F");
+            $(".fourthTemp").append("Temp: " + Math.round((((response.list[23].main.temp) - 273.15) * 1.80 + 32)) + " F");
+            $(".fifthTemp").append("Temp: " + Math.round((((response.list[31].main.temp) - 273.15) * 1.80 + 32)) + " F");
+
+            $(".firstHum").append("Humidity: " + response.list[0].main.humidity + "%");
+            $(".secondHum").append("Humidity: " + response.list[7].main.humidity + "%");
+            $(".thirdHum").append("Humidity: " + response.list[15].main.humidity + "%");
+            $(".fourthHum").append("Humidity: " + response.list[23].main.humidity + "%");
+            $(".fifthHum").append("Humidity: " + response.list[31].main.humidity + "%");
+        });
     emptyInfo();
 });
 
@@ -29,6 +61,24 @@ function emptyInfo() {
     $(".wind").empty();
     $(".humidity").empty();
     $(".temp").empty();
+
+    $(".firstDate").empty();
+    $(".secondDate").empty();
+    $(".thirdDate").empty();
+    $(".fourthDate").empty();
+    $(".fifthDate").empty();
+
+    $(".firstTemp").empty();
+    $(".secondTemp").empty();
+    $(".thirdTemp").empty();
+    $(".fourthTemp").empty();
+    $(".fifthTemp").empty();
+
+    $(".firstHum").empty();
+    $(".secondHum").empty();
+    $(".thirdHum").empty();
+    $(".fourthHum").empty();
+    $(".fifthHum").empty();
 }
 
 
